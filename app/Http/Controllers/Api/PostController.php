d7b50477d371a0bc0925c844571317f7557ba535
mukhtasar93 <mukhtasar03@gmail.com>
2023-06-20 13:22:05 +0700

    final databases and api backend

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new PostResource(Post::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // create validation
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //respon error validation
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+        
+        //save to database
+        $post = Post::create([
+            'title' => $request ->title,
+            'content' => $request ->content
+        ]);
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        //response error validation
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        //update to database
+        $post->update([
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        
+        return new PostResource($post);
+    }
+}
